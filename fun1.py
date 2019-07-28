fc32be8c21c901edf61fd3137ba0c8230fd99ab7
kenda21-meet <kenda21@meet.mit.edu>
2019-07-28 12:39:32 -0400

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,17 @@
+'''
+total = 0
+for number in range(1, 10 + 1):
+    print(number)
+    total = total + number
+print(total)
+'''
+
+def add_numbers():
+   total = 0
+   for number in range(1, 10 + 1):
+      return (number)
+   total = total + number
+   print(total)
+answer=add_numbers()
+print(answer)
+    
