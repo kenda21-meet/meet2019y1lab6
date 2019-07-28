5020e79caabb34333973b238007d835b891bceb8
kenda21-meet <kenda21@meet.mit.edu>
2019-07-28 13:06:23 -0400

    Stopping here

--- fun1.py
+++ fun1.py
@@ -6,12 +6,21 @@ for number in range(1, 10 + 1):
 print(total)
 '''
 
-def add_numbers():
-   total = 0
-   for number in range(1, 10 + 1):
-      return (number)
-   total = total + number
-   print(total)
-answer=add_numbers()
-print(answer)
-    
+##def add_numbers():
+##   total = 0
+##   for number in range(1, 10 + 1):
+##      print(number)
+##      total = total + number
+##   return(total)
+##answer=add_numbers()
+##print(answer)
+
+def my_function(x,y):
+    if x == 3:
+        #print(x)
+        return x
+    else:
+        return y
+        #print(y)
+my_function(12,3)
+#print(x)
