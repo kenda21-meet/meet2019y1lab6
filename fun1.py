d5a7c41d6fc1edcec4728aa7ecff859483f11d3d
kenda21-meet <kenda21@meet.mit.edu>
2019-07-29 03:08:04 -0400

    Stopping here

--- fun1.py
+++ fun1.py
@@ -5,16 +5,17 @@ for number in range(1, 10 + 1):
     total = total + number
 print(total)
 '''
-
-##def add_numbers():
-##   total = 0
-##   for number in range(1, 10 + 1):
-##      print(number)
-##      total = total + number
-##   return(total)
-##answer=add_numbers()
-##print(answer)
-
+'''
+def add_numbers():
+   total = 0
+   for number in range(1, 10 + 1):
+      print(number)
+      total = total + number
+   return(total)
+answer=add_numbers()
+print(answer)
+'''
+'''
 def my_function(x,y):
     if x == 3:
         #print(x)
@@ -24,3 +25,18 @@ def my_function(x,y):
         #print(y)
 my_function(12,3)
 #print(x)
+'''
+
+def add_numbers(start,end):
+    total=0
+    for number in range(start,end+1):
+        print(number)
+        total=total+number
+    return(total)
+test1=add_numbers(1,100)
+print(test1)
+        
+        
+        
+    
+
