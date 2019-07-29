6f5da482336bed67a0d183a8f0946913b63f1846
kenda21-meet <kenda21@meet.mit.edu>
2019-07-29 07:35:18 -0400

    Stopping here

--- fun2.py
+++ fun2.py
@@ -1,7 +1,32 @@
 import turtle
 turtle.goto(0,0)
+turtle.direction=None
+
 def up():
+    turtle.direction="Up"
     print("you pressed the up key.")
 turtle.onkey(up, "Up")
-turtle.goto(0,0)
+turtle.listen()
+
+def on_move():
+    x,y=turtle.pos()
+    if turtle.direction=="Up":
+        turtle.goto(x,y+100)
+def on_move2():
+    x,y=turtle.pos()
+    if turtle.direction=="Down":
+        turtle.goto(x,y-100)
+def on_move3():
+    x,y=turtle.pos()
+    if turtle.direction=="Right":
+        turtle.goto(x+100,y)
+def on_move4():
+    x,y=turtle.pos()
+    if turtle.direction=="Left":
+        turtle.goto(x-100,y)
+
+turtle.onkey(on_move,"Up")
+turtle.onkey(on_move2,"Down")
+turtle.onkey(on_move3,"Right")
+turtle.onkey(on_move4,"Left")
 turtle.listen()
