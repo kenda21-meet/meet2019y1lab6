a433e5b2f2569d04ef8b55dd55b5b91cba2a13f5
kenda21-meet <kenda21@meet.mit.edu>
2019-07-29 12:59:29 -0400

    Stopping here

--- fun2.py
+++ fun2.py
@@ -5,28 +5,38 @@ turtle.direction=None
 def up():
     turtle.direction="Up"
     print("you pressed the up key.")
+    on_move()
 turtle.onkey(up, "Up")
 turtle.listen()
+
+def down():
+    turtle.direction="Down"
+    on_move()
+turtle.onkey(down,"Down")
+turtle.listen()
+
+def right():
+    turtle.direction="Right"
+    on_move()
+turtle.onkey(right,"Right")
+turtle.listen()
+
+def left():
+    turtle.direction="Left"
+    on_move()
+turtle.onkey(left,"Left")
+turtle.listen()
+
+
 
 def on_move():
     x,y=turtle.pos()
     if turtle.direction=="Up":
         turtle.goto(x,y+100)
-def on_move2():
-    x,y=turtle.pos()
-    if turtle.direction=="Down":
+    elif turtle.direction=="Down":
         turtle.goto(x,y-100)
-def on_move3():
-    x,y=turtle.pos()
-    if turtle.direction=="Right":
+    elif turtle.direction=="Right":
         turtle.goto(x+100,y)
-def on_move4():
-    x,y=turtle.pos()
-    if turtle.direction=="Left":
+    elif turtle.direction=="Left":
         turtle.goto(x-100,y)
 
-turtle.onkey(on_move,"Up")
-turtle.onkey(on_move2,"Down")
-turtle.onkey(on_move3,"Right")
-turtle.onkey(on_move4,"Left")
-turtle.listen()
---
+++ fun3.py
@@ -0,0 +1,8 @@
+#print("*"*5)
+
+def draw_1d(n):
+    draw_1d(n*"*")
+print(draw_1d(3))
+    
+    
+
